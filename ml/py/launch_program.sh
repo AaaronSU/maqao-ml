76f43b46587a1b4389ea330a42a260381922de90
Aaron SU <aaron.su0424@gmail.com>
2024-06-25 12:14:15 +0200

    staged

--- ml/py/launch_program.sh
+++ ml/py/launch_program.sh
@@ -5,6 +5,8 @@ PYTHON_COMPILER="/usr/bin/python3"
 PYPY_COMPILER="/home/user/pycompilo/pypy3.10-v7.3.16-linux64/bin/pypy3"
 INTELPYTHON_COMPILER="/home/user/intelpython3/bin/python3.9"
 
+INTELPYTHON_OUTPUT_DIR="/home/user/macao-kitchen/ml/titanic-intelpython-report"
+
 echo "IntelPython is executing..."
 time ${INTELPYTHON_COMPILER} ${PROGRAM}
 echo "IntelPython finished execution."
@@ -18,3 +20,7 @@ echo
 echo "PyPy is executing..."
 time ${PYPY_COMPILER} ${PROGRAM}
 echo "PyPy finished execution."
+
+echo "Generating MAQAO report with intelpython compiler"
+maqao OV xp=${INTELPYTHON_OUTPUT_DIR} --replace -- ${INTELPYTHON_COMPILER} ${PROGRAM}
+echo "finished generate report"
